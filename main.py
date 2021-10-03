e6fafb8a304f799c5c95cba41fe0b507f16e5728
Misho <nnamper@gmail.com>
2021-10-03 09:42:01 +0400

    binding maybeY value to drop_zero function

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from maybe.maybe import Maybe
 from maybe.safe import safe, safe_return, do
 
+
 def safe_div_with_bind(mx: Maybe[int], my: Maybe[int]) -> Maybe[float]:
     return mx @ (lambda x: (
             my @ (lambda y: (safe_return(x / y)))
     ))
 
 
+def drop_zero(x: int) -> Maybe[float]:
+    return Maybe(x) if x != 0 else Maybe()
+
+
 @safe
 def div(mx: Maybe[int], my: Maybe[int]) -> Maybe[float]:
     x = do(mx)
-    y = do(my)
+    y = do(my @ drop_zero)
     return safe_return(x / y)
 
 
 def main():
-    mx = Maybe[int](3)
-    my = Maybe[int](10)
-    mz = Maybe[int]()
-    mr = safe_div_with_bind(my, mx)
-    mr_2 = div(my, mz)
-    print(mr, mr_2)
+    print(div(
+        Maybe[int](3), Maybe[int](0)
+    ))
+    print(div(
+        Maybe[int](9), Maybe[int](3)
+    ))
 
 
 if __name__ == '__main__':
